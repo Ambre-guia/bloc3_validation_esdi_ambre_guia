95f51a801e5acaeeef41743dd0413bef70c64a84
Ambre-guia <ambreguia77@gmail.com>
2025-06-05 14:48:44 +0200

    Update path

--- index.php
+++ index.php
@@ -3,9 +3,16 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
-require_once('config.php');
+require_once(__DIR__ .  '/config.php');
+require_once(__DIR__ . '/login/auth.php');
 
 $page = isset($_GET['page']) ? $_GET['page'] : 'home';
+
+// Rediriger vers books.php si l'utilisateur n'est pas admin
+if ($page === 'home' && !isAdmin()) {
+    header('Location: books.php');
+    exit;
+}
 
 if ($page === 'books') {
     include("books.php");
