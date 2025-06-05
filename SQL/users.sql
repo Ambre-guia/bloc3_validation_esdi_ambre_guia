9598c731e96c5f8086313d7f69cbe5e09a45f9f6
Ambre-guia <ambreguia77@gmail.com>
2025-06-05 14:31:48 +0200

    add user table

---
+++ SQL/users.sql
@@ -0,0 +1,16 @@
+-- Structure de la table `utilisateurs`
+CREATE TABLE `utilisateurs` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `prenom` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `mot_de_passe` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `date_inscription` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `role` enum('utilisateur','admin') COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'utilisateur',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Ajout d'un utilisateur admin par défaut (mot de passe: admin123)
+INSERT INTO `utilisateurs` (`nom`, `prenom`, `email`, `mot_de_passe`, `role`) VALUES
+('Admin', 'Admin', 'admin@librairie.com', '$2y$10$qm/Lo9UYK5NlLUQfMgGz5.YkT3q9EGCZrEBl.b3TYv5UbFMZXOE4.', 'admin');
