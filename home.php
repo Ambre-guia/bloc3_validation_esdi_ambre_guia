87189bf6f91093506926e851d1e673520ed72b7b
Ambre-guia <ambreguia77@gmail.com>
2025-06-05 14:43:28 +0200

    Update statistique

--- home.php
+++ home.php
@@ -1,5 +1,6 @@
 <?php
-    require_once('config.php');
+    require_once(__DIR__ . '/config.php');
+    require_once(__DIR__ . '/login/auth.php'); // Inclure le fichier d'authentification
 
 
 // Récupérer le nombre total de livres
@@ -7,6 +8,9 @@ $queryTotalBooks = "SELECT COUNT(*) as total_books FROM livres";
 $stmtTotalBooks = $pdo->prepare($queryTotalBooks);
 $stmtTotalBooks->execute();
 $resultTotalBooks = $stmtTotalBooks->fetch(PDO::FETCH_ASSOC);
+
+// Récupérer le nombre total d'utilisateurs en utilisant la fonction countUsers()
+$totalUsers = countUsers();
 ?>
 <!DOCTYPE html>
 <html>
@@ -26,11 +30,14 @@ $resultTotalBooks = $stmtTotalBooks->fetch(PDO::FETCH_ASSOC);
         <?php if (isset($_SESSION['user'])) : ?>
             <li>Bonjour <?= $_SESSION['prenom']; ?></li>
             <li><a href="books.php">Voir la liste des livres</a></li>
-            <li><a href="profile.php">Mon profil</a></li>
-            <li><a href="logout.php">Deconnexion</a></li>
+            <li><a href="login/profile.php">Mon profil</a></li>
+            <?php if ($_SESSION['role'] === 'admin') : ?>
+                <li><a href="admin.php">Administration</a></li>
+            <?php endif; ?>
+            <li><a href="login/logout.php">Deconnexion</a></li>
         <?php else : ?>
-            <li><a href="login.php">Connexion</a></li>
-            <li><a href="register.php">Inscription</a></li>
+            <li><a href="login/index.php">Connexion</a></li>
+            <li><a href="login/register.php">Inscription</a></li>
         <?php endif; ?>
     </ul>
 </nav>
@@ -54,7 +61,7 @@ $resultTotalBooks = $stmtTotalBooks->fetch(PDO::FETCH_ASSOC);
 
         <div class="statistic">
             <h3>Utilisateurs Enregistrés</h3>
-            <p><?php echo "Nombre d'utilisateur enregistré" ?></p>
+            <p><?php echo $totalUsers; ?></p>
         </div>
 
         <!-- ... Autres statistiques ... -->
