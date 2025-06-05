8c790ea896c228674eafa7e6cee04088725efedb
Ambre-guia <ambreguia77@gmail.com>
2025-06-05 14:43:47 +0200

    Path correction

--- login/auth.php
+++ login/auth.php
@@ -3,7 +3,7 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
-require_once('../config.php');
+require_once(__DIR__ . '/../config.php');
 
 /**
  * Vérifie si l'utilisateur est connecté
