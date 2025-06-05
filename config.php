d3b7d009ec1e634537b01ecbe4add141dbe237f9
Ambre-guia <ambreguia77@gmail.com>
2025-06-05 14:25:33 +0200

    add porojet to git

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+
+$host = 'localhost:3306';
+$dbname = 'exam3_esdi';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erreur de connexion à la base de données : " . $e->getMessage());
+}
